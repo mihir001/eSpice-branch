cbb3e80b678db078642c20c1558c3b1c9d10d4ea
Mihir Jain <jain.mihir2000@gmail.com>
2017-08-18 18:43:34 +0530

    Added language extensions in evaluate function

---
+++ src/cpp/eval.cpp
@@ -0,0 +1,122 @@
+#include "eval.hpp"
+#include "parse.hpp"
+#include "parse.cpp"
+
+/*
+    Copyright 2017 Yogesh Aggarwal
+    Evaluate Function
+    vector v stores the values at each point.
+*/
+
+
+std::vector<int>v;
+
+std::string stin = "" , stout = "" , stx = "";
+
+
+int command(node * , char , int, int, int);
+
+void loop(node *st, int ptr, int mxptr, int cur_size) {
+    while ( v[ptr] ) {
+        node *x = st; int cur = ptr;
+        while ( x->c != ']' ) {
+            cur = command(x , x->c , cur, mxptr , cur_size);
+            x = x->r;
+        }
+    }
+}
+
+
+/*
+    We use the concept of table doubling for managing memory
+*/
+
+
+void gr_table(int mxptr , int cur_size) {
+    if ( mxptr + 1 >= cur_size ) {
+        v.resize(2*cur_size);
+    } else if ( mxptr <= 1/4*cur_size ) {
+        v.resize(cur_size/2);
+    }
+}
+
+/* square, square root and factorial functions are accessed when their corresponding single character
+   i.e. 'A' for sq,'B' for sr, and 'C' for fact is encountered
+*/
+int square(int x){
+    return x*x;
+}
+int square_root(int x){
+    int t;
+    t = pow(x,0.5);
+    return t;
+}
+int factorial(int x){
+    int fact = 1;
+    while(x>0)
+    {
+        fact *= x;
+        x--;
+    }
+    return fact;
+}
+
+int command(node *cur, char c, int ptr, int mxptr, int cur_size) {
+    mxptr = std::max(ptr , mxptr);
+    gr_table(mxptr , cur_size);
+    if ( c == '+' ) {
+        v[ptr]++;
+        return ptr;
+    }
+    if ( c == '-' ) {
+        v[ptr]--;
+        return ptr;
+    }
+    if(c == 'A'){
+        v[ptr]=square(v[ptr]);
+        return ptr;
+    }
+    if(c == 'B'){
+        v[ptr]=square_root(v[ptr]);
+        return ptr;
+    }
+    if(c == 'C'){
+        v[ptr]=factorial(v[ptr]);
+        return ptr;
+    }
+    if ( c == '>' ) {
+        return ++ptr;
+    }
+    if ( c == '<' ) {
+        return --ptr;
+    }
+    if ( c == '.' ) {
+        printf("%c" , v[ptr]);
+        stout += v[ptr]; stx += v[ptr];
+        return ptr;
+    }
+    if ( c == ',' ) {
+        char l;
+        scanf("%c" , &l);
+        stin += l;
+        v[ptr] = l;
+        return ptr;
+    }
+    if ( c == '[' ) {
+        loop(cur->l , ptr, mxptr, cur_size);
+        return ptr;
+    }
+}
+
+void evaluate(node *cmd) {
+    node *cur = cmd;
+    int ptr = 0 , mxptr = 0 , cur_size = 0;
+    v.clear();
+    v.resize(10);
+    stin = "" ; stout = ""; stx = "";
+    while ( cur->r != NULL && cur->r != NULL ) {
+        ptr = command(cur , cur->c , ptr, mxptr, cur_size);
+        cur = cur->r;
+    }
+    command(cur , cur->c , ptr, mxptr , cur_size);
+}
--- src/cpp/eval.hpp
+++ src/cpp/eval.hpp
@@ -1,5 +1,5 @@
-#ifndef EVAL_INCLUDE
-#define EVAL_INCLUDE
+#ifndef EVAL_H_INCLUDED
+#define EVAL_H_INCLUDED
 
 /* The eval function takes as input an array "tokens" which contains multiple strings
  * This is represented by a pointer to an array of pointers to characters
@@ -7,7 +7,17 @@
  * It must process each token, perform the necessary operations as defined by the Brain**** language and by our extensions, printing any output along the way to the console
  * Finally, it must return the value at the position in memory where the data pointer is as an integer
  */
-int eval(char **tokens);
+
+#include <iostream>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<math.h>
+
+#include"parse.hpp"
+
+void evaluate(node*);
 
 int square(int x);
 int square_root(int x);
@@ -19,4 +29,5 @@ void mv(int a, int b);
 
 int *TABLE;
 
-#endif
+
+#endif // EVAL_H_INCLUDED
